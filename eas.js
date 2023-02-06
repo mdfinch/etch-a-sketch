a73635f056d145285a46240ebef3576bbbabafb8
Michael Finch <michael.donovan.finch@gmail.com>
2023-02-05 19:35:39 -0800

    Add event listener to change square on hover

--- eas.js
+++ eas.js
@@ -1,14 +1,15 @@
 let container = document.getElementsByClassName("container")[0];
 
 
-for (let index = 0; index < 5; index++) {
+for (let index = 0; index < 16; index++) {
     let row = document.createElement("div");
     row.className = `row-${index} row`;
-    container.appendChild(row)
+    container.appendChild(row);
 
-    for (let index = 0; index <5; index++) {
+    for (let index = 0; index < 16; index++) {
         let column = document.createElement("div");
-        column.className = `column-${index} column`
-        row.appendChild(column)
+        column.className = `column-${index} column`;
+        column.addEventListener("mouseover", function(){column.classList.add("yellow")})
+        row.appendChild(column);
     }
 }
