8ddce972ebc75544203353213fdfdb3be1d20c30
Michael Finch <michael.donovan.finch@gmail.com>
2023-02-12 12:45:49 -0800

    Add ability to display slider values
    Also added comments to help clarify.

--- eas.js
+++ eas.js
@@ -1,15 +1,31 @@
+// Gets container element
 let container = document.getElementsByClassName("container")[0];
 
+// Gets grid element
+let grid = document.getElementsByClassName("grid")[0];
 
-for (let index = 0; index < 16; index++) {
+// Gets slider element and shows their values
+let slider = document.getElementById("slider");
+let output = document.getElementById("value");
+let output2 = document.getElementById("value2");
+output.innerHTML = slider.value;
+output2.innerHTML = slider.value;
+
+// Creates initial grid
+// All rows are individually named to allow for seperate columns
+// A generic row class is used for styling
+for (let index = 0; index < slider.value; index++) {
     let row = document.createElement("div");
+    
     row.className = `row-${index} row`;
-    container.appendChild(row);
-
-    for (let index = 0; index < 16; index++) {
+    grid.appendChild(row);
+    
+    for (let index = 0; index < slider.value; index++) {
         let column = document.createElement("div");
+        // All columns are individually named to allow for seperate event listeners
+        // A generic column class is used for styling
         column.className = `column-${index} column`;
-        column.addEventListener("mouseover", function(){column.classList.add("yellow")})
+        column.addEventListener("mouseover", function(){column.classList.add("color")})
         row.appendChild(column);
     }
 }
